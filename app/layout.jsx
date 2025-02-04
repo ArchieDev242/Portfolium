af32a27d362b2aa3e09ae1fd8a36edf38e57a27a
ArchieDev242 <maxkopychko@gmail.com>
2025-02-05 00:03:41 +0200

    continued

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const press = Press_Start_2P({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "400",
+  variable: '--font-press-start'
 });
 
 export const metadata = {
@@ -19,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={press.variable}>
         {children}
       </body>
     </html>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-};
+	darkMode: ["class"],
+	content: [
+	  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	prefix: "",
+	theme: {
+	  container: {
+		center: true,
+		padding: "15px",
+	  },
+	  screens: {
+		sm: "640px",
+		md: "768px",
+		lg: "1024px",
+		xl: "1280px"
+	  },
+	  extend: {
+		fontFamily: {
+		  primary: ["var(--font-press-start)", "monospace"] // Виправлено синтаксис
+		},
+		colors: {
+		  background: 'var(--background)',
+		  foreground: 'var(--foreground)'
+		},
+		borderRadius: {
+		  lg: 'var(--radius)',
+		  md: 'calc(var(--radius) - 2px)',
+		  sm: 'calc(var(--radius) - 4px)'
+		}
+	  }
+	},
+	plugins: [require("tailwindcss-animate")]
+  }
+  
