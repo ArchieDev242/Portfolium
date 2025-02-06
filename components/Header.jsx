d06c7f297203e4fe6ee6a05eb528eeaed85da56a
ArchieDev242 <maxkopychko@gmail.com>
2025-02-06 13:55:57 +0200

    some updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,20 +8,25 @@ import Nav from "./Nav";
 
 const Header = () => {
     return(
-    <header className="py-8 xl:py12 text-white bg-pink-50/20">
-        <div className="container mx-auto">
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
             {/* our logo */}
             <Link href="/"> 
-                <h1 className="text-4xl font-semibold">Maksym <span className="text-accent">.</span></h1>
+                <h1 className="text-4xl font-semibold">Maksym <span className="text-accent-default">.</span></h1>
             </Link>
 
             {/* nav + btn hire */}
-            <div className="hidden xl:flex">
+            <div className="hidden xl:flex items-center gap-8">
                 <Nav />
                 <Link href="/contacts">
                     <Button>Hire Me</Button>
                 </Link>
             </div>
+
+            {/* mobile port */}
+
+            <div className="xl:hidden">Mobile Nav</div>
+
         </div>
     </header>
     );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,41 @@
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "Skills",
+        path: "/skills",
+    },
+    {
+        name: "Projects",
+        path: "/projects",
+    },
+    {
+        name: "Resume",
+        path: "/resume",
+    },
+    {
+        name: "Contacts",
+        path: "/contacts",
+    },
+] 
 
 const Nav = () => {
-    return (
-        <nav className="">Desktop Nav</nav>
-    )
+
+    const navigation = usePathname();
+    console.log(navigation);
+
+    return <nav className="flex gap-8">
+        {links.map((link, index) => { 
+            return <Link href={link.path} key={index} className="{``}">{link.name}</Link>
+        })}
+    </nav>
 }
 
 export default Nav;
