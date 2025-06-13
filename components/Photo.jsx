8bbfae3f534b3edd6a02f4b309bc14f80107473a
ArchieDev242 <maxkopychko@gmail.com>
2025-06-14 00:33:52 +0300

    photo fix x2

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,47 +5,47 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center relative">
+    <div className = "w-full h-full flex justify-center items-center relative">
       {/* Image */}
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
+        initial = {{ opacity: 0 }}
+        animate = {{
           opacity: 1,
           transition: { delay: 2, duration: 0.2, ease: "easeInOut" },
         }}
-        className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] rounded-full overflow-hidden absolute"
+        className = "w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] rounded-full overflow-hidden absolute"
       >
         <Image
-          src="/assets/kitty.jpg"
+          src = "./assets/kitty.jpg"
           priority
-          quality={100}
+          quality = {100}
           fill
-          alt="kitty image"
-          className="object-cover"
+          alt = "kitty image"
+          className = "object-cover"
         />
       </motion.div>
 
       {/* Circle animation */}
       <motion.svg
-        className="w-[320px] xl:w-[520px] h-[320px] xl:h-[520px]"
-        fill="transparent"
-        viewBox="0 0 520 520"
-        xmlns="http://www.w3.org/2000/svg"
+        className = "w-[320px] xl:w-[520px] h-[320px] xl:h-[520px]"
+        fill = "transparent"
+        viewBox = "0 0 520 520"
+        xmlns = "http://www.w3.org/2000/svg"
       >
         <motion.circle
-          cx="260"
-          cy="260"
-          r="250"
-          stroke="var(--accent-default)"
-          strokeWidth="4"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          initial={{ strokeDasharray: "30 20", strokeDashoffset: 0 }}
-          animate={{
+          cx = "260"
+          cy = "260"
+          r = "250"
+          stroke = "var(--accent-default)"
+          strokeWidth = "4"
+          strokeLinecap = "round"
+          strokeLinejoin = "round"
+          initial = {{ strokeDasharray: "30 20", strokeDashoffset: 0 }}
+          animate = {{
             strokeDashoffset: [-200, 0],
             rotate: [0, 360],
           }}
-          transition={{
+          transition = {{
             ease: "linear",
             duration: 4,
             repeat: Infinity,
